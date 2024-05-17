8b4b4f2b9095c9da042d50046f6ba357de9160ed
SriVardhini26 <136246525+SriVardhini26@users.noreply.github.com>
2024-05-17 17:18:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+let rcbImageEl = document.getElementById("rcblogo");
+let cskImageEl = document.getElementById("csklogo");
+let srhImageEl = document.getElementById("srhlogo");
+let miImageEl = document.getElementById("milogo");
+let gtImageEl = document.getElementById("gtlogo");
+let dcImageEl = document.getElementById("dclogo");
+let kkrImageEl = document.getElementById("kkrlogo");
+let pbksImageEl = document.getElementById("pbkslogo");
+let rrImageEl = document.getElementById("rrlogo");
+let rcbImage = "https://th.bing.com/th/id/OIP.wFtXBZrRA7C_17BCopqb-QAAAA?rs=1&pid=ImgDetMain";
+let cskImage = "https://e0.pxfuel.com/wallpapers/634/157/desktop-wallpaper-ruturaj-gaikwad-chennai-super-kings-csk-ipl-2021-chennai-super-kings-ms-dhoni-mumbai-indians-ipl.jpg";
+let srhImage = "https://th.bing.com/th/id/OIP.3xQHS3z6XE_MONi7wddOWAHaEK?rs=1&pid=ImgDetMain";
+let miImage = "https://wallpapercave.com/wp/wp7285698.jpg";
+let gtImage = "https://th.bing.com/th/id/OIP.jdafpSIuXpWbjLoFKO8mQAHaJ3?rs=1&pid=ImgDetMain";
+let dcImage = "https://wallpapercave.com/wp/wp9025441.jpg";
+let kkrImage = "https://pragativadi.com/wp-content/uploads/2021/03/Shreyas-Iyer.jpg";
+let pbksImage = "https://staticimg.amarujala.com/assets/images/2022/11/02/750x506/shikhar-dhawan_1667407405.jpeg";
+let rrImage = "https://c.ndtvimg.com/2023-05/fni0bpdo_sanju-samson-bcci_625x300_08_May_23.jpg?im=FaceCrop,algorithm=dnn,width=806,height=605";
+
+function clickRcb() {
+    rcbImageEl.src = rcbImage;
+}
+
+function clickCsk() {
+    cskImageEl.src = cskImage;
+}
+
+function clickSrh() {
+    srhImageEl.src = srhImage;
+}
+
+function clickMi() {
+    miImageEl.src = miImage;
+}
+
+function clickGt() {
+    gtImageEl.src = gtImage;
+}
+
+function clickDc() {
+    dcImageEl.src = dcImage;
+}
+
+function clickKkr() {
+    kkrImageEl.src = kkrImage;
+}
+
+function clickPbks() {
+    pbksImageEl.src = pbksImage;
+}
+
+function clickRr() {
+    rrImageEl.src = rrImage;
+}
